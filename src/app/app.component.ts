f7d4abdfe99077a7e159e5f689c147630f896a3a
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-07-20 14:59:39 +0700

    Revert "use input"
    This reverts commit ec79ef83170d030050ed93a297662939e9b6f51b.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  firstname = '';
-  lastname = '';
-  result='';
-  colors = ['red', 'blue', 'green'];
-  
-  inputYourName(event: any) : void {
-    this.firstname = event.target.value;
-    
-  }
-
-  processFullname(): void {
-    this.result = this.firstname + " " + this.lastname
-  }
-
-  inputLastname(lastname: string): void {
-    this.lastname = lastname;
-  }
+  title = 'hello';
 }
